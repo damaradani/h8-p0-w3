e1db2327e8376d01ad8cbd04f118e516233468dd
Dani Damara <danny12march@gmail.com>
2018-02-08 00:30:47 +0700

    menambahkan fungsi drawThreeColBox() baru untuk angka parameter sampai 1

--- senin-w3/live-code-1-1/rocket.js
+++ senin-w3/live-code-1-1/rocket.js
@@ -48,15 +48,11 @@ for (height; height > 0; height--){
 }
 }
 
-drawThreeColBox(4);
-drawThreeColBox2(5);
-
-
+//Function tanpa menggunakan array dan angka di mulai dari 1 sampai height2
 function drawThreeColBox2(height2){
 
   //var rows = height2*3;
   var number = 1;
-  var tampung = '';
   for( var i = 0; i < height2; i++){
     var hasil = '';
 
@@ -68,3 +64,26 @@ function drawThreeColBox2(height2){
     console.log(hasil);
   }
 }
+
+//Function untuk angka height3 sampai 1
+function drawThreeColBox3(height3){
+  var number = height3*3;
+
+  for(var i = 0; i <height3; i++){
+    var hasil3 = '';
+
+    for(var j = 0; j < 3; j++ ){
+      hasil3 += number+' ';
+      number--;
+    }
+
+    console.log(hasil3);
+  }
+}
+
+//Memanggil fungsi
+drawThreeColBox(4);
+console.log('============');
+drawThreeColBox2(5);
+console.log('============');
+drawThreeColBox3(6);
