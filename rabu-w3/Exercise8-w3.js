a98a321c6c66227c20378dd65243a8919a683bfe
Dani Damara <danny12march@gmail.com>
2018-02-09 15:31:48 +0700

    Mengganti coding dengan yang lebih rapi

--- rabu-w3/Exercise8-w3.js
+++ rabu-w3/Exercise8-w3.js
@@ -1,22 +1,19 @@
 //Exercise 8 week 3 Logic Challenge - Pasangan Angka Terbesar
 
 function pasanganTerbesar(num){
-    var arrNum = '';
-    arrNum += num;
-    var Indeks1 = 0;
-    var Indeks2 = 1;
-    var arrHasil = [];
 
-    for(var i = Indeks2; i < arrNum.length; i++){
-      arrHasil.push(arrNum[Indeks1]+arrNum[Indeks2]);
-      //arrHasil.push(arrNum[Indeks2]);
+  var Indeks1 = 1;
+  var numStr = String(num);
+  var arrNum = [];
+
+  for (i = 0; i < (numStr.length-1); i++){
+      arrNum.push(numStr[i]+numStr[Indeks1]);
       Indeks1++;
-      Indeks2++;
-    }
-    var hasilSort = arrHasil.sort(function(v1,v2){return v1 > v2}).map(Number);
-    return  typeof(hasilSort[hasilSort.length -1]);
-}
+  }
 
+  var arrNumSort = arrNum.sort(function(a,b) {return a < b});
+  return arrNumSort[0];
+}
 
 // TEST CASES
 console.log(pasanganTerbesar(641573)); // 73
